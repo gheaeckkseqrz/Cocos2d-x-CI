1986b49bcbf56f1683d301b9584f63a90f7586d0
Pierre Wilmot <gheaeckkseqrz@gmail.com>
2019-07-19 10:32:50 +0100

    Setup

--- signCmakeList.sh
+++ signCmakeList.sh
@@ -1,2 +1,2 @@
-echo "set_xcode_property(\${APP_NAME} DEVELOPMENT_TEAM \"E5P9GHJFJ8\")" >> ./CMakeLists.txt
-echo "set_target_properties(${APP_NAME} PROPERTIES XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY \"iPhone Developer: Pierre WILMOT\")" >> ./CMakeLists.txt
+#echo "set_xcode_property(\${APP_NAME} DEVELOPMENT_TEAM \"E5P9GHJFJ8\")" >> ./CMakeLists.txt
+#echo "set_target_properties(${APP_NAME} PROPERTIES XCODE_ATTRIBUTE_CODE_SIGN_IDENTITY \"iPhone Developer: Pierre WILMOT\")" >> ./CMakeLists.txt
